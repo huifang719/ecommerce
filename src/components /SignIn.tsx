faaf13c10a9dcee6fb5578930c84a087a0462756
huifang <hui-f.gu@hotmail.com>
2023-02-15 12:16:18 +1030

    set error message

--- src/components /SignIn.tsx
+++ src/components /SignIn.tsx
@@ -25,14 +25,14 @@ const SignIn: React.FC = () => {
         <Form.Text style={{ fontSize: '2rem'}}>Sign In</Form.Text>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" name="email" value={formState.email} onChange={handleInputChange} />
+          <Form.Control type="email" placeholder="Enter email" name="email" value={formState.email} onChange={handleInputChange} required />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" name="password" value={formState.password} onChange={handleInputChange} />
+          <Form.Control type="password" placeholder="Password" name="password" value={formState.password} onChange={handleInputChange} required />
         </Form.Group>
         <Button variant="primary" type="submit">
           Sign In
--- src/components /SignUp.tsx
+++ src/components /SignUp.tsx
@@ -31,20 +31,21 @@ const SignUp: React.FC = () => {
         <Form.Text style={{ fontSize: '2rem'}}>Sign Up</Form.Text>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" value={formState.email} name="email" onChange={handleInputChange} />
+          <Form.Control type="email" placeholder="Enter email" value={formState.email} name="email" onChange={handleInputChange} required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" value={formState.password} name="password"
-          onChange={handleInputChange} />
+          onChange={handleInputChange} required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Comfirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Comfirm Password" value={formState.comfirmPassword} name="comfirmPassword" onChange={handleInputChange} />
+          <Form.Control type="password" placeholder="Comfirm Password" value={formState.comfirmPassword} name="comfirmPassword" onChange={handleInputChange} required />
         </Form.Group>
         <Button variant="primary" type="submit">
           Sign Up
         </Button>
+        <h5 style={errorMessage===null? {display:"none"}: {color:"rgb(110,223,94)"}}>{errorMessage}</h5>
       </Form>
     </>
   )

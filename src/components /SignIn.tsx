77c1e56c0d7dd69acf64a761a707f01e8760ab80
huifang <hui-f.gu@hotmail.com>
2023-02-15 11:47:11 +1030

    signin / signup form setup

--- src/components /SignIn.tsx
+++ src/components /SignIn.tsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Button }from 'react-bootstrap';
 
+interface FormState {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
+  const [formState, setFormState] = useState<FormState>({ email: '', password: '' });
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    setFormState({ ...formState, [name]: value });
+  };
+  const handleSignIn = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    console.log(formState)
+  }
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSignIn}>
         <Form.Text style={{ fontSize: '2rem'}}>Sign In</Form.Text>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" name="email" value={formState.email} onChange={handleInputChange} />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" name="password" value={formState.password} onChange={handleInputChange} />
         </Form.Group>
         <Button variant="primary" type="submit">
           Sign In
--- src/components /SignUp.tsx
+++ src/components /SignUp.tsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Form, Button }from 'react-bootstrap';
 
+interface FormState {
+  email: string;
+  password: string;
+  comfirmPassword: string
+}
+
 const SignUp: React.FC = () => {
+  const [formState, setFormState] = useState<FormState>({ email: '', password: '', comfirmPassword: '' });
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+  
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormState({ ...formState, [name]: value });
+  };
+
+  const handleSignUp = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    if (formState.password !== formState.comfirmPassword) {
+      setErrorMessage('Password does not match')
+    } else {
+      console.log(formState)
+    }
+    
+  }
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSignUp}>
         <Form.Text style={{ fontSize: '2rem'}}>Sign Up</Form.Text>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" value={formState.email} name="email" onChange={handleInputChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" value={formState.password} name="password"
+          onChange={handleInputChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Comfirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Comfirm Password" />
+          <Form.Control type="password" placeholder="Comfirm Password" value={formState.comfirmPassword} name="comfirmPassword" onChange={handleInputChange} />
         </Form.Group>
         <Button variant="primary" type="submit">
           Sign Up

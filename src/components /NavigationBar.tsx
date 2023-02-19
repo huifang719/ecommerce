7b17a94faadaaad94a92fd58b8cf4c4481d9e698
huifang <hui-f.gu@hotmail.com>
2023-02-19 20:41:30 +1030

    navbar adjustment

--- src/components /NavigationBar.tsx
+++ src/components /NavigationBar.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { Navbar, Nav, Form, FormControl, Button, Container } from "react-bootstrap";
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Navbar, Nav, Form, FormControl, Button, Container, Row, Col } from "react-bootstrap";
 import {
-  FaFacebook, FaInstagram, FaTwitter,
+  FaFacebook, FaInstagram, FaTwitter, FaCartArrowDown
 } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 import { Link } from 'react-router-dom';
+import "../App.css"
 
 const NavigationBar: React.FC = () => {
   const [expanded, setExpanded] = useState(false);
@@ -15,28 +15,37 @@ const NavigationBar: React.FC = () => {
         <Navbar.Brand href="#home">E-Commerce Website</Navbar.Brand>
         <Navbar.Toggle onClick={() => setExpanded(expanded ? false : true)} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="me-auto justify-content-space-evenly" style={{ width: '50%' }} >
             <Nav.Link onClick={() => setExpanded(false)} as={Link} to="/">Home</Nav.Link>
             <Nav.Link onClick={() => setExpanded(false)} href="#products">Products</Nav.Link>
             <Nav.Link onClick={() => setExpanded(false)} as={Link} to="./members">Members</Nav.Link>
           </Nav>
-          <Form className="d-inline-flex">
+          <Form className="d-inline-flex search">
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-success">Search</Button>
           </Form>
-          <IconContext.Provider value={{ size: '1.5rem', color: 'black' }}>
-          <Breadcrumb className="justify-content-end">
-            <Breadcrumb.Item href="/">
-              <FaFacebook />
-            </Breadcrumb.Item>
-            <Breadcrumb.Item href="https://github.com/huifang719">
-              <FaInstagram />
-            </Breadcrumb.Item>
-            <Breadcrumb.Item href="https://www.linkedin.com/in/liviagu/">
-              <FaTwitter />
-            </Breadcrumb.Item>
-          </Breadcrumb>
-        </IconContext.Provider>
+            <Row>
+              <Col xs={8} lg={6} md={6} className="cart">
+                <IconContext.Provider value={{ size: '2.5rem', color: 'black' }}>
+                  <FaCartArrowDown href="/" />
+                </IconContext.Provider>
+              </Col>
+              <Col xs={4} lg={6} md={6} style={{alignSelf: "flex-end"}} >
+                <Container className="social" style={{ width:100 }}>
+                  <IconContext.Provider value={{ size: '1rem', color: 'black' }}>
+                    <Col>
+                      <a href="./"><FaFacebook /></a>
+                    </Col>
+                    <Col>
+                      <a href="https://www.instagram.com/lightstory_photography/"><FaInstagram /></a>
+                    </Col>
+                    <Col>
+                      <a href="https://twitter.com/Lightstory_foto"><FaTwitter /></a>
+                    </Col>
+                  </IconContext.Provider>
+                </Container>
+              </Col>
+            </Row>
         </Navbar.Collapse>
       </Container>
     </Navbar>
